f3f38197c3d39a6ad7fd8184912de0c58d8b0fa0
Govind Thakur <govindthakur25@gmailcom>
2025-09-16 22:04:07 +0530

    feat: Migrate task controller to the new repository pattern

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,41 +1,30 @@
 import { Request, Response } from "express";
-import prisma from "~/prisma-client";
-import EntityNotFoundError from "~/errors/EntityNotFoundError";
+import { repository } from "~/data/repositories";
 
 export const getAllTasks = async (req: Request, res: Response) => {
-  const tasks = await prisma.task.findMany({
-    where: {
-      user_id: req.auth?.payload.sub,
-    },
-  });
+  const tasks = await repository.listTasks({}, req.auth?.payload.sub);
   res.status(200).json({ tasks });
 };
 
 export const getTaskById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const task = await prisma.task.findUnique({
-    where: {
-      id,
-      user_id: req.auth?.payload.sub,
-    },
-  });
-  if (!task) {
-    throw new EntityNotFoundError({
-      message: `Task with id ${id} not found`,
-      statusCode: 404,
-      code: "ERR_NF",
-    });
-  }
+  const task = await repository.getTask(id, req.auth?.payload.sub);
   res.status(200).json({ task });
 };
 
-export const createTask = (req: Request, res: Response) => {
-  res.status(201).json({ message: "Create task", data: req.body });
+export const createTask = async (req: Request, res: Response) => {
+  const task = await repository.createTask(req.body, req.auth?.payload.sub);
+  res.status(201).json({ task });
 };
 
 export const updateTask = (req: Request, res: Response) => {
   const { id } = req.params;
-  res.status(200).json({ message: `Update task ${id}`, data: req.body });
+  const updatedTask = repository.updateTask(
+    id,
+    req.body,
+    req.auth?.payload.sub,
+  );
+  res.status(200).json({ task: updateTask });
 };
 
 export const deleteTask = (req: Request, res: Response) => {
